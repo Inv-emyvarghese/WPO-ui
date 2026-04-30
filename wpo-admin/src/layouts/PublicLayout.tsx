6628b9cce0d49c29b6e14306ee63cee14d356f70
emy.varghese <emy.varghese@innovaturelabs.com>
2026-04-30 12:43:55 +0530

    login page style is changed

--- wpo-admin/src/layouts/PublicLayout.tsx
+++ wpo-admin/src/layouts/PublicLayout.tsx
@@ -1,19 +1,12 @@
-// import Header from "@/components/layout/Header";
-// import Footer from "@/components/layout/Footer";
 import type React from "react";
 import { Outlet } from "react-router-dom";
 
 const PublicLayout: React.FC = () => {
-  /** No-op while sidebar is disabled; restore drawer state when `Sidebar` is re-enabled. */
-  // const handleDrawerToggle = () => {};
-
   return (
-    <div className="flex min-h-dvh min-h-[100svh] w-full min-w-0 flex-1 flex-col">
-      {/* <Header handleDrawerToggle={handleDrawerToggle} /> */}
-      <div className="min-h-0 w-full min-w-0 flex-1 overflow-auto bg-slate-100 pt-16">
+    <div className="flex min-h-dvh min-h-[100svh] w-full min-w-0 flex-1 flex-col bg-white">
+      <div className="flex min-h-0 w-full min-w-0 flex-1 flex-col items-center justify-center overflow-y-auto bg-white px-4 py-8">
         <Outlet />
       </div>
-      {/* <Footer /> */}
     </div>
   );
 };
